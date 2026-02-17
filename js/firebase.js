c2746db56f4b01d8f9100a7d052565cf73ccfd7b
pdanttas04-blip <pdanttas04@icloud.com>
2026-02-16 21:52:14 -0300

    Atualizar o firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 
 // Configuração do seu projeto
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "AIzaSyCuxohEgAldsWE5N-WDkRcY1lJ0I3CBgFw",
   authDomain: "cartola-fc-pro.firebaseapp.com",
   projectId: "cartola-fc-pro",
-  storageBucket: "cartola-fc-pro.appspot.com",
+  storageBucket: "cartola-fc-pro.firebasestorage.app",
   messagingSenderId: "1041176031687",
-  appId: "SEU_APP_ID"
+  appId: "1:1041176031687:web:5c37488402cb9be5eb29e2"
 };
 
-// Inicializa
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
