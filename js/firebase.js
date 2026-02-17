2f1ffb27b3bbf5e518d71338be3af8c2c89c69d0
pdanttas04-blip <pdanttas04@icloud.com>
2026-02-16 21:44:03 -0300

    Atualizar o firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,8 @@
+// Importações do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Configuração do seu projeto
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
   authDomain: "cartola-fc-pro.firebaseapp.com",
@@ -16,41 +12,8 @@ const firebaseConfig = {
   appId: "SEU_APP_ID"
 };
 
+// Inicializa
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// REGISTRAR
-window.registrar = function(email, senha) {
-  createUserWithEmailAndPassword(auth, email, senha)
-    .then(() => {
-      alert("Conta criada!");
-      window.location.href = "premium.html";
-    })
-    .catch(error => alert(error.message));
-}
-
-// LOGIN
-window.login = function(email, senha) {
-  signInWithEmailAndPassword(auth, email, senha)
-    .then(() => {
-      alert("Login realizado!");
-      window.location.href = "premium.html";
-    })
-    .catch(error => alert(error.message));
-}
-
-// PROTEGER PÁGINA PREMIUM
-if (window.location.pathname.includes("premium.html")) {
-  onAuthStateChanged(auth, user => {
-    if (!user) {
-      window.location.href = "login.html";
-    }
-  });
-}
-
-// LOGOUT
-window.logout = function() {
-  signOut(auth).then(() => {
-    window.location.href = "index.html";
-  });
-}
+export { auth };
