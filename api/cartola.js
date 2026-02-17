a2bdbadc164a31dc6bfd29af52dedb9831e9a74d
pdanttas04-blip <pdanttas04@icloud.com>
2026-02-17 00:30:56 -0300

    Atualizar o cartola.js

--- api/cartola.js
+++ api/cartola.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://api.cartola.globo.com/atletas/mercado");
+    const response = await fetch("https://api.cartola.globo.com/mercado/status");
     const data = await response.json();
 
     res.status(200).json(data);
